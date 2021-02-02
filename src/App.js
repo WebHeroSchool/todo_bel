7958857acf1d9d781f205fd08d3fb824cc9ebc2a
Евгения Белоярцева <evgeniya.kornako@mail.ru>
2021-02-02 19:41:32 +0300

    create todo

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+
+const index = 27;
+const name = 'Санкт-Петербург';
+const sum = 10+5;
+let com;
+if (sum==15) com= true;
+let elem = false;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+         <p style={{
+          color: 'red'
+          }}>
+         Hello </p>
+         <p>
+          Вывод числа: {index}
+         </p>
+         <p> 
+          Вывод значения переменной : {name}
+         </p> 
+         <p>
+           Вывод результата арифметической операции: {sum}
+         </p>
+         <p>
+            Вывод результата логической операции: {com && 'true'}
+         </p> 
+         <p> 
+           Использования тернарного условия: {elem ? 'elem true' : 'elem false'}
+         </p>
+         <p>
+           {undefined} {null} {false} {true} 
+         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
