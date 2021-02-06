9af5aacdbcca1a17122dc14b859bc1015014dd02
Евгения Белоярцева <evgeniya.kornako@mail.ru>
2021-02-06 20:21:14 +0300

    props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react'; 
 import ItemList from '../ItemList/ItemList';
+import Footer from '../Footer/Footer';
 
+const todoItem = 'Написать сайт';
 const App = () => (
 	<div> 
 	  <h1> Важные дела </h1> 
-	  <ItemList />
+	  <ItemList todoItem={todoItem} />
+	  <Footer count={10} />
 	</div>);
 
 	export default App;
