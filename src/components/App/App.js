af94fc39624977ad2837d344f034af085c9beb91
Евгения Белоярцева <evgeniya.kornako@mail.ru>
2021-02-10 20:30:13 +0300

    styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,8 @@ import Footer from '../Footer/Footer';
 import InputItem from '../InputItem/InputItem';
 import styles from  './App.module.css';
 import Paper from '@material-ui/core/Paper';
+import FilterList from '../FilterList/FilterList';
+import Divider from '@material-ui/core/Divider';
 
 const todoItem = 'Написать сайт';
 
@@ -28,8 +30,11 @@ const App = () => {
 	  <h1 className={styles.title}> Важные дела </h1> 
 	  <InputItem />
 	  <ItemList items={items} />
-	  <Footer count={10} />
-	</div>
+	 </div>
+	 <div> 
+	 <Divider />
+	   <Footer count={10} />
+	 </div>
 	</Paper> );
 }
 
